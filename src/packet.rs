2154256329bceecd5137f5dad74eb97a151e8133
black-binary <blackbinary@qq.com>
2022-01-02 01:10:40 +0800

    refactor: refine PacketPoller trait

--- src/packet.rs
+++ src/packet.rs
@@ -14,37 +14,18 @@ use crate::Error;
 use crate::MAX_FRAME_LEN;
 
 pub trait PacketPoller {
-    type MetaInfo: Clone + PartialEq;
-
-    fn poll_send_to(
-        &self,
-        cx: &mut Context<'_>,
-        buf: &[u8],
-        meta: Self::MetaInfo,
-    ) -> Poll<std::io::Result<usize>>;
-
-    fn poll_recv_from(
-        &self,
-        cx: &mut Context<'_>,
-        buf: &mut ReadBuf<'_>,
-    ) -> Poll<std::io::Result<Self::MetaInfo>>;
+    fn poll_send(&self, cx: &mut Context<'_>, buf: &[u8]) -> Poll<std::io::Result<usize>>;
+    fn poll_recv(&self, cx: &mut Context<'_>, buf: &mut ReadBuf<'_>) -> Poll<std::io::Result<()>>;
 }
 
-async fn recv_from<P: PacketPoller<MetaInfo = M>, M: Clone>(
-    p: &P,
-    buf: &mut [u8],
-) -> std::io::Result<(usize, M)> {
+async fn recv<P: PacketPoller>(p: &P, buf: &mut [u8]) -> std::io::Result<usize> {
     let mut read_buf = ReadBuf::new(buf);
-    let meta = poll_fn(|cx| p.poll_recv_from(cx, &mut read_buf)).await?;
-    Ok((read_buf.filled().len(), meta))
+    poll_fn(|cx| p.poll_recv(cx, &mut read_buf)).await?;
+    Ok(read_buf.filled().len())
 }
 
-async fn send_to<P: PacketPoller<MetaInfo = M>, M: Clone>(
-    p: &P,
-    buf: &[u8],
-    m: M,
-) -> std::io::Result<usize> {
-    let n = poll_fn(|cx| p.poll_send_to(cx, buf, m.clone())).await?;
+async fn send<P: PacketPoller>(p: &P, buf: &[u8]) -> std::io::Result<usize> {
+    let n = poll_fn(|cx| p.poll_send(cx, buf)).await?;
     Ok(n)
 }
 
@@ -65,7 +46,6 @@ impl<T: PacketPoller> NoisePacket<T> {
     pub async fn handshake(
         inner: T,
         mut state: HandshakeState,
-        meta: T::MetaInfo,
         timeout: Duration,
         mut max_retry: usize,
     ) -> Result<Self, Error> {
@@ -80,12 +60,12 @@ impl<T: PacketPoller> NoisePacket<T> {
                 last_sent.resize(MAX_FRAME_LEN, 0);
                 let n = state.write_message(&[], &mut last_sent)?;
                 last_sent.truncate(n);
-                send_to(&inner, &last_sent, meta.clone()).await?;
+                send(&inner, &last_sent).await?;
             } else {
                 let mut recv_buf = vec![0; MAX_FRAME_LEN];
 
                 let result =
-                    tokio::time::timeout(timeout, async { recv_from(&inner, &mut recv_buf).await })
+                    tokio::time::timeout(timeout, async { recv(&inner, &mut recv_buf).await })
                         .await;
 
                 if max_retry == 0 {
@@ -94,17 +74,14 @@ impl<T: PacketPoller> NoisePacket<T> {
 
                 match result {
                     Ok(r) => {
-                        let (n, m) = r?;
-                        if m != meta {
-                            continue;
-                        }
+                        let n = r?;
                         let mut payload_buf = vec![0; MAX_FRAME_LEN];
                         state.read_message(&recv_buf[..n], &mut payload_buf)?;
                     }
                     Err(_) => {
                         max_retry -= 1;
                         if !last_sent.is_empty() {
-                            send_to(&inner, &last_sent, meta.clone()).await?;
+                            send(&inner, &last_sent).await?;
                         }
                     }
                 }
@@ -112,21 +89,21 @@ impl<T: PacketPoller> NoisePacket<T> {
         }
     }
 
-    pub async fn send_to(&self, buf: &[u8], meta: T::MetaInfo) -> Result<usize, Error> {
+    pub async fn send(&self, buf: &[u8]) -> Result<usize, Error> {
         let mut message = vec![0; 8 + MAX_FRAME_LEN];
         let nonce: u64 = rand::thread_rng().gen();
         message[..8].copy_from_slice(&nonce.to_le_bytes());
         let n = self
             .transport
             .write_message(nonce, buf, &mut message[8..])?;
-        poll_fn(|cx| self.inner.poll_send_to(cx, &message[..8 + n], meta.clone())).await?;
+        poll_fn(|cx| self.inner.poll_send(cx, &message[..8 + n])).await?;
         Ok(buf.len())
     }
 
-    pub async fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, T::MetaInfo), Error> {
+    pub async fn recv(&self, buf: &mut [u8]) -> Result<usize, Error> {
         let mut recv_buf = vec![0; 8 + MAX_FRAME_LEN];
         let mut read_buf = ReadBuf::new(&mut recv_buf);
-        let meta = poll_fn(|cx| self.inner.poll_recv_from(cx, &mut read_buf)).await?;
+        poll_fn(|cx| self.inner.poll_recv(cx, &mut read_buf)).await?;
 
         let mut cur = read_buf.filled();
         let nonce = cur.get_u64_le();
@@ -134,14 +111,13 @@ impl<T: PacketPoller> NoisePacket<T> {
 
         let n = self.transport.read_message(nonce, message, buf)?;
 
-        Ok((n, meta))
+        Ok(n)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use std::{
-        net::SocketAddr,
         task::{Context, Poll},
         time::Duration,
     };
@@ -151,23 +127,15 @@ mod tests {
     use crate::{NoisePacket, PacketPoller};
 
     impl PacketPoller for UdpSocket {
-        type MetaInfo = SocketAddr;
-
-        fn poll_send_to(
-            &self,
-            cx: &mut Context<'_>,
-            buf: &[u8],
-            meta: Self::MetaInfo,
-        ) -> Poll<std::io::Result<usize>> {
-            self.poll_send_to(cx, buf, meta)
+        fn poll_send(&self, cx: &mut Context<'_>, buf: &[u8]) -> Poll<std::io::Result<usize>> {
+            self.poll_send(cx, buf)
         }
-
-        fn poll_recv_from(
+        fn poll_recv(
             &self,
             cx: &mut Context<'_>,
             buf: &mut ReadBuf<'_>,
-        ) -> Poll<std::io::Result<Self::MetaInfo>> {
-            self.poll_recv_from(cx, buf)
+        ) -> Poll<std::io::Result<()>> {
+            self.poll_recv(cx, buf)
         }
     }
 
@@ -179,6 +147,9 @@ mod tests {
         let c = UdpSocket::bind("127.0.0.1:0").await.unwrap();
         let c_addr = c.local_addr().unwrap();
 
+        c.connect(s_addr).await.unwrap();
+        s.connect(c_addr).await.unwrap();
+
         let initiator = snow::Builder::new(PATTERN.parse().unwrap())
             .build_initiator()
             .unwrap();
@@ -187,17 +158,17 @@ mod tests {
             .unwrap();
 
         tokio::spawn(async move {
-            let a = NoisePacket::handshake(s, initiator, c_addr, Duration::from_secs(1), 3)
+            let a = NoisePacket::handshake(s, initiator, Duration::from_secs(1), 3)
                 .await
                 .unwrap();
-            a.send_to(b"hello world!", c_addr).await.unwrap();
+            a.send(b"hello world!").await.unwrap();
         });
 
-        let b = NoisePacket::handshake(c, responder, s_addr, Duration::from_secs(1), 3)
+        let b = NoisePacket::handshake(c, responder, Duration::from_secs(1), 3)
             .await
             .unwrap();
         let mut buf = vec![0; 0x100];
-        let (n, _) = b.recv_from(&mut buf).await.unwrap();
+        let n = b.recv(&mut buf).await.unwrap();
         let s = String::from_utf8_lossy(&buf[..n]);
         println!("{}", s);
     }
