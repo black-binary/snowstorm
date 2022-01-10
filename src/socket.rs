484a8d62b30477052ba556779c5e583182d3ed87
black-binary <blackbinary@qq.com>
2022-01-10 21:54:11 +0800

    refactor: optimize gettime

--- src/socket.rs
+++ src/socket.rs
@@ -1,7 +1,11 @@
 use std::collections::VecDeque;
 use std::fmt::Debug;
+use std::sync::atomic::AtomicU32;
+use std::sync::atomic::Ordering;
+use std::sync::Once;
 use std::task::Context;
 use std::task::Poll;
+use std::time::Duration;
 
 use bytes::Buf;
 use futures_util::future::poll_fn;
@@ -43,8 +47,15 @@ async fn send<P: PacketPoller>(p: &mut P, buf: &[u8]) -> std::io::Result<usize> 
     Ok(n)
 }
 
-#[inline]
-fn now() -> u32 {
+static TIMESTAMP: AtomicU32 = AtomicU32::new(0);
+static TIMESTAMP_WORKER: Once = Once::new();
+
+#[inline(always)]
+fn timestamp() -> u32 {
+    TIMESTAMP.load(Ordering::Relaxed)
+}
+
+fn timestamp_realtime() -> u32 {
     coarsetime::Clock::now_since_epoch().as_secs() as u32
 }
 
@@ -116,6 +127,16 @@ impl<T: PacketPoller> NoiseSocket<T> {
         mut state: HandshakeState,
         verifier: &mut V,
     ) -> Result<Self, Error> {
+        TIMESTAMP_WORKER.call_once(|| {
+            TIMESTAMP.store(timestamp_realtime(), Ordering::Relaxed);
+            tokio::spawn(async {
+                let interval = Duration::from_millis(200);
+                loop {
+                    TIMESTAMP.store(timestamp_realtime(), Ordering::Relaxed);
+                    tokio::time::sleep(interval).await;
+                }
+            });
+        });
         let mut peer_time = 0;
         let mut buf = vec![0; MAX_MESSAGE_LEN];
         loop {
@@ -139,7 +160,7 @@ impl<T: PacketPoller> NoiseSocket<T> {
             }
 
             if state.is_my_turn() {
-                let n = state.write_message(&now().to_le_bytes(), &mut buf)?;
+                let n = state.write_message(&timestamp().to_le_bytes(), &mut buf)?;
                 send(&mut inner, &buf[..n]).await?;
             } else {
                 let n = recv(&mut inner, &mut buf).await?;
@@ -174,7 +195,7 @@ impl<T: PacketPoller> NoiseSocket<T> {
             return Err(Error::MalformedPacket("message too long".into()));
         }
 
-        self.send_payload_buf[..TIMESTAMP_LEN].copy_from_slice(&now().to_le_bytes());
+        self.send_payload_buf[..TIMESTAMP_LEN].copy_from_slice(&timestamp().to_le_bytes());
         self.send_payload_buf[TIMESTAMP_LEN..TIMESTAMP_LEN + len].copy_from_slice(buf);
 
         let nonce = self.rng.next_u64();
@@ -358,7 +379,7 @@ mod tests {
             .unwrap();
         let a = h.await.unwrap();
 
-        let t = super::now();
+        let t = super::timestamp();
 
         // Replay
         for _ in 0..2 {
